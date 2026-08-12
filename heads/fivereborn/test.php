9e26a155c0f5ba8e6346fb8fa57b9fefa4ca401f
HIEN <hphien444@users.noreply.github.com>
2026-08-12 21:13:10 +0700

    Update WESTSIDE TEST launcher to 1000052

--- heads/fivereborn/test.php
+++ heads/fivereborn/test.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000050');
-header('x-amz-meta-bootstrap-size: 5106960');
-header('x-amz-meta-bootstrap-object: d75fded697266c102cf2c030b16e18a59f0b5fb49cb696c814a63cda627b26e5');
-header('x-amz-meta-branch-version: 1786512061');
-header('x-amz-meta-branch-manifest: 1164f17893aee38077a937624193003f70e1d9beb90f2def2e995736d58b4265');
+header('x-amz-meta-bootstrap-version: 1000052');
+header('x-amz-meta-bootstrap-size: 5106724');
+header('x-amz-meta-bootstrap-object: a94a6e8b882554b5ba07f36a7122e8bc523d9db0a38bf8405ff1a72b833b42a6');
+header('x-amz-meta-branch-version: 1786542697');
+header('x-amz-meta-branch-manifest: 1108c0d756458706a20c20b0cd5aea3373076588f6d07a8def840de48ae5b309');
 echo "OK";
 ?>
