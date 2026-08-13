f5e286849ee1d1f11b18b56cca8d23f8cf5cf3b7
HIEN <hphien444@users.noreply.github.com>
2026-08-13 16:30:07 +0700

    Harden WESTSIDE TEST DLL loading policy to 1000055

--- heads/fivereborn/test.php
+++ heads/fivereborn/test.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000054');
-header('x-amz-meta-bootstrap-size: 5106672');
-header('x-amz-meta-bootstrap-object: 703480bf187e5ef3399439d6f14b35f6f6c098cfbcabd16c65ffe4de83c8ff8a');
-header('x-amz-meta-branch-version: 1786611329');
-header('x-amz-meta-branch-manifest: 5a07c2299382287cf4fdccf5b1fdfd3d032c8ed75504235ae640932a7ce9e2c8');
+header('x-amz-meta-bootstrap-version: 1000055');
+header('x-amz-meta-bootstrap-size: 5107068');
+header('x-amz-meta-bootstrap-object: 6b7fb401d8bc78406e8eba2ef05a38f39e7154211a7c8da3ed402a82840367dc');
+header('x-amz-meta-branch-version: 1786613256');
+header('x-amz-meta-branch-manifest: 0d584db96751f02c8d6ab42a5f86a23bf46c83b43ec9e3f28051382a30453939');
 echo "OK";
 ?>
