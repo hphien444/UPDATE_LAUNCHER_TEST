51e6acb27969c94dfb30c00a19d8fc2d555e1cab
HIEN <hphien444@users.noreply.github.com>
2026-08-25 02:00:44 +0700

    Publish WESTSIDE TEST client version 1000064

--- heads/fivereborn/test.php
+++ heads/fivereborn/test.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000063');
-header('x-amz-meta-bootstrap-size: 5107756');
-header('x-amz-meta-bootstrap-object: a1dbe0f89b96fcf791d3c4bf4a7012b5d723a1d68876072e5e45235aaf6088f6');
-header('x-amz-meta-branch-version: 1787596037');
-header('x-amz-meta-branch-manifest: bcbc5c85dc3bea778cb7d8b34b4d24bf4cf9aa5bb915058120c4797f1aa499ee');
+header('x-amz-meta-bootstrap-version: 1000064');
+header('x-amz-meta-bootstrap-size: 5107176');
+header('x-amz-meta-bootstrap-object: 1f0c12d8b3c29ad5eb64933f331fafc691b6fe0c36d32760a26a20ee8826cfb4');
+header('x-amz-meta-branch-version: 1787597959');
+header('x-amz-meta-branch-manifest: 97aee899ad773bf9128b3368bd0dc18060160438ba507a7379adfa5bfa681439');
 echo "OK";
 ?>
